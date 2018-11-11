affe6749f3c19d4f14f9626e9532614031f075b9
Obafemi Fadairo <ofpupp@princeton.edu>
2018-11-11 00:45:10 +0000

    automatically backed up by learn

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,6 +1,6 @@
 class PostSerializer
   def self.serialize(post)
-   serialized_post = '{'
+    serialized_post = '{'
     serialized_post += '"id": ' + post.id.to_s + ', '
     serialized_post += '"title": "' + post.title + '", '
     serialized_post += '"description": "' + post.description + '", '
