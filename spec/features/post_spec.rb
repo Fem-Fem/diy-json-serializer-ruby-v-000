832d6bbe4630c459536269a85a72428e39af8002
Obafemi Fadairo <ofpupp@princeton.edu>
2018-11-11 00:51:43 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_helper'
-
-describe 'navigate' do
+# require 'rails_helper'
+#
+# describe 'navigate' do
 #   before do
 #     @post = Post.create(title: "My Post", description: "My post desc", name: "Steven")
 #   end
